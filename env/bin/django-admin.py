00efcc98e363197711ccf7da9d149e957392da0e
Izat Turganaliev <izatturganaliev@Izats-MacBook-Air.local>
2020-06-05 23:30:43 +0600

    My Django Boys app, first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/izatturganaliev/Documents/python_projects/web_development/djangoboys/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
